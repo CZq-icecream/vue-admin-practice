3e3d1f49989a6beaa50d3f459e057af2576a2b4f
CZq-icecream <137577743@qq.com>
2021-07-30 07:14:11 +0800

    day4

---
+++ day4/views/nested/menu1/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div style="padding:30px;">
+    <el-alert :closable="false" title="menu 1">
+      <router-view />
+    </el-alert>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
